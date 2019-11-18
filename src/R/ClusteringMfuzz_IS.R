6634489841be73c238d5d52ed92eace6e18da9ea
Iryna Shutava <iryna.shutava@slu.se>
2019-11-18 08:27:16 +0100

    Mfuzz analysis

---
+++ src/R/ClusteringMfuzz_IS.R
@@ -0,0 +1,138 @@
+#' ---
+#' title: "T89 and _Laccaria bicolor_ soft clustering with Mfuzz"
+#' author: "Nicolas Delhomme && Iryna Shutava"
+#' date: "`r Sys.Date()`"
+#' output:
+#'  html_document:
+#'    toc: true
+#'    number_sections: true
+#' ---
+#' # Setup
+#' * Working directory
+setwd("/mnt/picea/projects/aspseq/jfelten/T89-Laccaria-bicolor")
+#' ```{r set up, echo=FALSE}
+#' knitr::opts_knit$set(root.dir="/mnt/picea/projects/aspseq/jfelten/T89-Laccaria-bicolor")
+#' ```
+
+#' * Libraries
+suppressPackageStartupMessages(library(data.table))
+suppressPackageStartupMessages(library(DESeq2))
+suppressPackageStartupMessages(library(gplots))
+suppressPackageStartupMessages(library(hyperSpec))
+suppressPackageStartupMessages(library(LSD))
+suppressPackageStartupMessages(library(parallel))
+suppressPackageStartupMessages(library(pander))
+suppressPackageStartupMessages(library(plotly))
+suppressPackageStartupMessages(library(RColorBrewer))
+suppressPackageStartupMessages(library(scatterplot3d))
+suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(vsn))
+library(Mfuzz)
+library(cluster)
+#library(fpc)
+
+#' * Helper functions
+source("~/Git/UPSCb-common/src/R/plot.multidensity.R")
+source("~/Git/UPSCb-common/src/R/featureSelection.R")
+
+#' * Graphics
+pal <- brewer.pal(8,"Dark2")
+hpal <- colorRampPalette(c("blue","white","red"))(100)
+mar <- par("mar")
+
+load("analysis/salmon/Potri-all-dds.rda")
+
+vsd <- varianceStabilizingTransformation(dds,blind=FALSE)
+vst <- assay(vsd)
+vst <- vst - min(vst)
+
+#Potri <- new('ExpressionSet', exprs=as.matrix(vst))
+sel <- dds$Experiment=="ECM"
+eset <- ExpressionSet(sapply(split.data.frame(t(vst[,sel]),dds$Time[sel]),colMeans))
+
+
+
+
+
+#f.eset <- filter.std(eset,min.std=0.1)
+#s.eset <- standardise(f.eset)
+#m1 <- mestimate(s.eset)
+#cl=mfuzz(s.eset,c=24,m=m1)
+
+Potri <- eset
+
+#' Filtered it. 
+#' NB: Works only with "NA", not with "0" value -> in some cases we need to exclude NA genes after standardise
+#' 
+Potri.r <- filter.NA(Potri, thres=0.25)
+#Potri.f <- fill.NA(Potri.r,mode="mean")
+
+Potri.f <- filter.std(Potri.r,min.std=0.1)
+
+Potri.s <- standardise(Potri.f)
+
+#' We need to exclude NA genes.
+Potri.s <- filter.NA(Potri.s, thres=0.25)
+
+test <- Potri.s@assayData[["exprs"]]
+
+#' # Soft clustering of gene expression data.
+#' ## Initial step - N of the Clusters = 16 
+cl <- mfuzz(Potri.s, c=16, m=1.25)
+
+m1 <- mestimate(Potri.s)
+m1
+
+#ecm <- getCluster(vst,dds$Experiment=="ECM")
+
+#' ### Plot clustering results
+#' 
+clusplot(test, cl$cluster)
+points(cl$center,col=1:16,pch=8,cex=1)
+
+#mfuzz.plot2(Potri.s,cl=cl,mfrow=c(4,4),x11=FALSE) # same output as mfuzz.plot
+mfuzz.plot2(Potri.s, cl=cl,mfrow=c(4,4),centre=TRUE,x11=FALSE) # lines for cluster centres will be included
+
+#' Extracting list of genes belonging to the cluster cores
+list_genes <- acore(eset,cl,min.acore=0.5)
+
+#' ### Overlapping of the clusters
+#' 
+O <- overlap(cl)
+Ptmp <- overlap.plot(cl,over=O,thres=0.05)
+
+#' ## Soft clustering for the N of the clusters = 3
+#' 
+cl3 <- mfuzz(Potri.s, c=3, m=1.25)
+
+#' ### Plot clustering results
+#' 
+clusplot(test, cl3$cluster)
+points(cl3$center,col=1:16,pch=8,cex=1)
+
+#mfuzz.plot2(Potri.s,cl=cl3,mfrow=c(4,4),x11=FALSE) # same output as mfuzz.plot
+mfuzz.plot2(Potri.s, cl=cl3,mfrow=c(4,4),centre=TRUE,x11=FALSE) # lines for cluster centres will be included
+
+#' ### Overlapping of the clusters
+#' 
+O3 <- overlap(cl3)
+Ptmp <- overlap.plot(cl3,over=O3,thres=0.05)
+
+
+
+
+# --------
+#' # k-means clustering 
+#' 
+kl <- kmeans(test, centers = 10, iter.max = 100)
+clusplot(test, kl$cluster)
+points(kl$center,col=1:10,pch=8,cex=1)
+
+clusplot(test, kl$cluster, color=TRUE, shade=TRUE, 
+         labels=2, lines=0)
+
+kl2 <- kmeans2(Potri.s,k=16)
+clusplot(test, kl2$cluster)
+points(kl$center,col=1:16,pch=8,cex=1)
+
+
--- src/R/Mfuzz.R
+++ src/R/Mfuzz.R
@@ -15,14 +15,16 @@ suppressPackageStartupMessages(library(Mfuzz))
 
 #' * Helpers
 source(here("UPSCb-common/src/R/featureSelection.R"))
+#source("~/Git/UPSCb-common/src/R/featureSelection.R")
 
 #' * Data
-load(here("data/analysis/salmon/Potri-all-dds.rda"))
+#load(here("analysis/salmon/Potri-all-dds.rda"))
+load("/mnt/picea/projects/aspseq/jfelten/T89-Laccaria-bicolor/analysis/salmon/Potri-all-dds.rda")
 
 #' * Function
 getCluster <- function(vst,sample.sel,min.std=0.1,clusterN=24){
   
-  eset <- ExpressionSet(sapply(split.data.frame(t(vst[,sample.sel]),dds$Time),colMeans))
+  eset <- ExpressionSet(sapply(split.data.frame(t(vst[,sample.sel]),dds$Time[sample.sel]),colMeans))
   
   f.eset <- filter.std(eset,min.std=min.std)
   
@@ -38,34 +40,110 @@ vsd <- varianceStabilizingTransformation(dds,blind=FALSE)
 vst <- assay(vsd)
 vst <- vst - min(vst)
 
-save(vst,file=here("data/analysis/DE/Potri_variance-stabilised-transformed_model-aware_data.rda"))
+#save(vst,file=here("data/analysis/DE/Potri_variance-stabilised-transformed_model-aware_data.rda"))
 
 #' # Clustering
 #' ## ECM
 ecm <- getCluster(vst,dds$Experiment=="ECM")
-save(cl,s.eset,file="cl.rda")
 
-#mfuzz.plot(s.eset,cl=cl,mfrow=c(4,4),time.labels=seq(0,160,10))
+#save(ecm$cl,ecm$eset,file="cl.rda")
 
-barplot(table(cl$cluster),las=2)
+#' ### Overlapping of the clusters
+O <- overlap(ecm$cl)
+Ptmp <- overlap.plot(ecm$cl,over=O,thres=0.05)
+
+#' ### Plot clustering results
+barplot(table(ecm$cl$cluster),las=2)
+
+mfuzz.plot2(ecm$eset,cl=ecm$cl,mfrow=c(4,4),x1=FALSE)
+
+
 
 source("~/Git/UPSCb-common/src/R/gopher.R")
 
-enr.list <- lapply(split(rownames(s.eset),cl$cluster),
-    gopher,background=rownames(s.eset),
+enr.list <- lapply(split(rownames(ecm$eset),ecm$cl$cluster),
+    gopher,background=rownames(ecm$eset),
     task="go",url="potri")
 
-# run also for Control (add overlap)
+#ND: run also for Control (add overlap)
+#' ## Control
+cont <- getCluster(vst,dds$Experiment=="Cont")
 
-# run for ECM - Cont (calculate the ratio ECM - Control)
-rat$eset <- exprs(ecm$eset) - exprs(cont$eset)
+#save(cont$cl,cont$eset,file="cl.rda")
+
+#' ### Overlapping of the clusters
+O <- overlap(cont$cl)
+Ptmp <- overlap.plot(cont$cl,over=O,thres=0.05)
+
+#' ### Plot clustering results
+barplot(table(cont$cl$cluster),las=2)
+
+mfuzz.plot2(cont$eset,cl=cont$cl,mfrow=c(4,4),x1=FALSE)
+
+
+enr.list.cont <- lapply(split(rownames(cont$eset),cont$cl$cluster),
+                   gopher,background=rownames(cont$eset),
+                   task="go",url="potri")
+
+
+#ND: run for ECM - Cont (calculate the ratio ECM - Control)
+#' ## ECM - Control
+#IS: - ?
+#rat$eset <- exprs(ecm$eset) - exprs(cont$eset)
 
 # run comparison of member genes accross the ECM and Cont clusters
-# as you said, overlap :-)
+# as you said, overlap
+
+#' ## Comparison of member genes accross the ECM and Cont clusters
+#' ### ECM's clusters
+#' 
+#' 
+#ecm$cl$membership[ecm$cl$membership > 0.7]
+
+acore.list.ecm <- acore(ecm$eset,cl=ecm$cl,min.acore=0.7)
+acore.ecm = list(1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4)
+
+for(i in 1:24){
+  acore.ecm[i] <- length(row.names(acore.list.ecm[[i]]))
+}
+
+barplot(t(as.matrix(acore.ecm)),las=2,
+        main = "Genes belonging to the cluster cores, alpha=0.7, ECM",
+        xlab = "Clusters",
+        ylab = "Number of genes",
+        names.arg = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24),
+        col = "darkred")
+
+#' plot
+boxplot(lapply(1:length(ecm$cl$size),function(i){
+  ecm$cl$membership[ecm$cl$cluster==i,i]}))
+
+#' ### Control's clusters
+acore.list.cont <- acore(cont$eset,cl=cont$cl,min.acore=0.7)
+
+#vector(mode="integer",length=24)
+#acore.cont = list(1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3,4)
+
+#for(i in 1:24){
+#  acore.cont[i] <- length(row.names(acore.list.cont[[i]]))
+#}
+
+acore.cont <- sapply(acore.list.cont,nrow)
+
+barplot(acore.cont)
+
+barplot(t(as.matrix(acore.cont)),las=2,
+        main = "Genes belonging to the cluster cores, alpha=0.7, control",
+        xlab = "Clusters",
+        ylab = "Number of genes",
+        names.arg = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24),
+        col = "darkred")
 
 # perform the same on the laccaria 
 # redo the whole analysis
 # take the data from here("data/analysis/DE/Lacbi2....)
+
+load("/mnt/picea/projects/aspseq/jfelten/T89-Laccaria-bicolor/analysis/salmon/Lacbi-all-dds.rda")
 
 # run the full network and use guide genes from T89 and Laccaria
 # let's talk when you get there ;-)
